35dd2048ae89b7f9feaae80fef774045bbe0b2bc
Stanislav Jendrišák <st.jendrisakgmail.com>
2022-07-24 13:20:30 +0200

    Pridal som funcionalitu k fotogalerii.

--- script.js
+++ script.js
@@ -18,3 +18,42 @@ function deleteActive() {
         e.classList.remove("active");
     })
 }
+
+// ===== FOTOGALERIA ================================
+
+let fotogalleryImg = document.querySelectorAll(".fotogaleria img");
+let body             = document.querySelector("body");
+
+console.log( fotogalleryImg )
+
+for(let i = 0; fotogalleryImg.length > i; i++) {
+    fotogalleryImg[i].addEventListener("click", function(e) {
+
+        console.log( fotogalleryImg[i] )
+
+        //modal
+        var modal = document.createElement("div");
+        modal.classList.add("modal");
+
+        // img
+        let imger = document.createElement("img");
+            imger.setAttribute("src", this.src);
+            imger.classList.add("imger");
+
+        // Pridanie img a modalu
+        body.appendChild(modal);
+        modal.appendChild(imger);
+
+            //Vynazaie modalu
+        document.addEventListener("keyup", function(e){
+        if(e.code === "Escape"){
+          modal.remove();
+        }
+     });
+
+     modal.addEventListener("click", function(e){
+        modal.remove()
+     });
+
+    });
+};
